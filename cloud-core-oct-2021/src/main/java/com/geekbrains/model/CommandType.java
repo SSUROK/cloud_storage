a5085f6025d7f20b965c3deed8be35d2210550ac
Ilay Vinogradov <ilay125114@gmail.com>
2021-11-06 21:59:10 +0300

    Done#2

--- cloud-core-oct-2021/src/main/java/com/geekbrains/model/CommandType.java
+++ cloud-core-oct-2021/src/main/java/com/geekbrains/model/CommandType.java
@@ -1,8 +1,11 @@
 package com.geekbrains.model;
 
-public enum CommandType {
+public enum CommandType{
     FILE_MESSAGE,
     FILE_REQUEST,
     LIST_MESSAGE,
-    LIST_REQUEST
+    LIST_REQUEST,
+    FILE_DELETE,
+    SERVER_OFFLINE,
+    SERVER_ONLINE
 }
